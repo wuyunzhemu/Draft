a5f9fb4890f42e365d4cc5989d2f39307c2e7205
wuyunzhemu <415159489@qq.com>
2018-10-07 20:49:46 +0800

    新增了JS实现继承

--- JS实现类的相关功能/JSClass.js
+++ JS实现类的相关功能/JSClass.js
@@ -1,8 +1,8 @@
 var people =function(name,age) {    //构造函数
   // var name=name;
   // var age=age;
-  // this.name=name;
-  // this.age=age;
+   this.name=name;
+   this.age=age;
   
   //  this.fun1 = function(){
   //    console.log(this.name+" is "+this.age+" years old");
@@ -18,5 +18,18 @@ var I = new people('wyzm',21);
 I.fun1();
 var U = new people('dztz',250);
 U.fun1();
-      
 //console.log(I.age);    //类外应该不允许直接访问类的属性,在构造函数用var声明属性，则为私有，用this.xx 则为公有,而类名.xx则为静态属性
+
+
+
+
+var girl = function(){                            //实现继承效果
+ this.loveMe=true;
+}
+girl.prototype = new people('girl1',21);          //此处略有缺陷，应在原型，即父类内定义赋值函数
+girl.prototype.answerMe = function(){
+  return this.loveMe;
+}
+
+var aGirl = new girl();
+console.log(aGirl.name +'   ' +aGirl.answerMe());
